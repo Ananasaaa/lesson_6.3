1fd1673faa1dab512850a55a4169f88c584a9281
Ananasaaa <kairyakn@gmail.com>
2024-06-07 17:24:14 +0300

    lesson_6.3

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+const arr = [2, 7, 5, 8, 0, 7, 4];
+
+
+function removeElement(array, item) {
+   return array.filter((element) => element !== item);
+}
+
+
+
+ let res = removeElement(arr, 7);
+console.log(res);
+
+
+
+
+
+
+
